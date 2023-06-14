9a35deabdde38753e95a358383004dbc159e6d8d
Mohamed Karim Ben Boubaker <63352044+Med-Karim-Ben-Boubaker@users.noreply.github.com>
2023-06-14 21:58:45 +0100

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,8 @@
 // <vector> - Adjacency List representation
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 using namespace std;
 
 // Graph class using adjacency list representation
@@ -22,15 +25,72 @@ public:
     adj[v].push_back(u);
   }
   
-  // Print the adjacency list representation of the graph
-  void printGraph() {
-    for (int v = 0; v < V; ++v) {
-      cout << "Adjacency list of vertex " << v << ": ";
-      for (const auto& vertex : adj[v]) {
-        cout << vertex << " ";
+  // Utility function for DFS
+  void DFSUtil(int v, vector<bool>& visited) {
+    visited[v] = true;
+    cout << v << " ";
+    
+    for (int neighbor : adj[v]) {
+      if (!visited[neighbor]) {
+        DFSUtil(neighbor, visited);
       }
-      cout << endl;
     }
+  }
+
+  // Depth-First Search (DFS)
+  void DFS(int start) {
+    vector<bool> visited(V, false);  // Mark all vertices as not visited
+    DFSUtil(start, visited);
+  }
+  
+  
+  // Breadth-First Search (BFS)
+  void BFS(int start) {
+    vector<bool> visited(V, false);  // Mark all vertices as not visited
+    queue<int> q;                    // Create a queue for BFS
+    visited[start] = true;           // Mark the current node as visited
+    q.push(start);                   // Enqueue the current node
+    
+    while (!q.empty()) {
+      int v = q.front();             // Dequeue a vertex from the queue
+      cout << v << " ";
+      q.pop();
+      
+      // Get all adjacent vertices of the dequeued vertex
+      // If a adjacent has not been visited, mark it visited and enqueue it
+      for (int neighbor : adj[v]) {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          q.push(neighbor);
+        }
+      }
+    }
+  }
+  // Dijkstra's algorithm
+  vector<int> dijkstra(int start) {
+    vector<int> dist(V, INT_MAX);         // Initialize distances as infinity
+    dist[start] = 0;                      // Distance of start vertex is 0
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start});                   // Enqueue the start vertex
+    
+    while (!pq.empty()) {
+      int u = pq.top().second;             // Get the vertex with the minimum distance
+      pq.pop();
+      
+      for (const auto& neighbor : adj[u]) {
+        int v = neighbor.first;
+        int weight = neighbor.second;
+        
+        // If there is a shorter path to v through u
+        if (dist[u] + weight < dist[v]) {
+          dist[v] = dist[u] + weight;
+          pq.push({dist[v], v});
+        }
+      }
+    }
+    
+    return dist;
   }
 };
 
@@ -47,70 +107,22 @@ int main() {
   g.addEdge(2, 3);
   g.addEdge(3, 4);
   
-  // Print the adjacency list representation of the graph
-  g.printGraph();
+  cout << "DFS traversal starting from vertex 0: ";
+  g.DFS(0);
+  cout << endl;
   
-  return 0;
-}
-
-
-// <array> - Adjacency Matrix representation
-#include <array>
-using namespace std;
-
-// Graph class using adjacency matrix representation
-class Graph {
-  int V;                       // Number of vertices
-  
-  // 2D array to represent the adjacency matrix
-  array<array<int, 100>, 100> adj;
-  
-public:
-  // Constructor
-  Graph(int vertices) {
-    V = vertices;
-    adj.fill({});
-  }
-  
-  // Add an edge to the graph
-  void addEdge(int u, int v) {
-    adj[u][v] = 1;
-    adj[v][u] = 1;
-  }
-  
-  // Print the adjacency matrix representation of the graph
-  void printGraph() {
-    for (int i = 0; i < V; ++i) {
-      for (int j = 0; j < V; ++j) {
-        cout << adj[i][j] << " ";
-      }
-      cout << endl;
-    }
-  }
-};
-
-int main() {
-  // Create a graph with 5 vertices
-  Graph g(5);
-  
-  // Add edges
-  g.addEdge(0, 1);
-  g.addEdge(0, 4);
-  g.addEdge(1, 2);
-  g.addEdge(1, 3);
-  g.addEdge(1, 4);
-  g.addEdge(2, 3);
-  g.addEdge(3, 4);
-  
-  // Print the adjacency matrix representation of the graph
-  g.printGraph();
+  cout << "BFS traversal starting from vertex 0: ";
+  g.BFS(0);
+  cout << endl;
   
   return 0;
 }
 
 
 // <unordered_map> - Adjacency List representation with weights
+#include <iostream>
 #include <unordered_map>
+#include <queue>
 using namespace std;
 
 // Graph class using adjacency list representation with weights
@@ -132,15 +144,71 @@ public:
     adj[v][u] = weight;
   }
   
-  // Print the adjacency list representation of the graph with weights
-  void printGraph() {
-    for (const auto& vertex : adj) {
-      cout << "Adjacency list of vertex " << vertex.first << ": ";
-      for (const auto& edge : vertex.second) {
-        cout << edge.first << "(" << edge.second << ") ";
+  // Depth-First Search (DFS)
+  void DFS(int start) {
+    vector<bool> visited(V, false);  // Mark all vertices as not visited
+    DFSUtil(start, visited);
+  }
+  
+  // Utility function for DFS
+  void DFSUtil(int v, vector<bool>& visited) {
+    visited[v] = true;
+    cout << v << " ";
+    
+    for (const auto& neighbor : adj[v]) {
+      if (!visited[neighbor.first]) {
+        DFSUtil(neighbor.first, visited);
       }
-      cout << endl;
     }
+  }
+  
+  // Breadth-First Search (BFS)
+  void BFS(int start) {
+    vector<bool> visited(V, false);  // Mark all vertices as not visited
+    queue<int> q;                    // Create a queue for BFS
+    visited[start] = true;           // Mark the current node as visited
+    q.push(start);                   // Enqueue the current node
+    
+    while (!q.empty()) {
+      int v = q.front();             // Dequeue a vertex from the queue
+      cout << v << " ";
+      q.pop();
+      
+      // Get all adjacent vertices of the dequeued vertex
+      // If a adjacent has not been visited, mark it visited and enqueue it
+      for (const auto& neighbor : adj[v]) {
+        if (!visited[neighbor.first]) {
+          visited[neighbor.first] = true;
+          q.push(neighbor.first);
+        }
+      }
+    }
+  }
+  // Dijkstra's algorithm
+  vector<int> dijkstra(int start) {
+    vector<int> dist(V, INT_MAX);         // Initialize distances as infinity
+    dist[start] = 0;                      // Distance of start vertex is 0
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start});                   // Enqueue the start vertex
+    
+    while (!pq.empty()) {
+      int u = pq.top().second;             // Get the vertex with the minimum distance
+      pq.pop();
+      
+      for (const auto& neighbor : adj[u]) {
+        int v = neighbor.first;
+        int weight = neighbor.second;
+        
+        // If there is a shorter path to v through u
+        if (dist[u] + weight < dist[v]) {
+          dist[v] = dist[u] + weight;
+          pq.push({dist[v], v});
+        }
+      }
+    }
+    
+    return dist;
   }
 };
 
@@ -157,8 +225,13 @@ int main() {
   g.addEdge(2, 3, 60);
   g.addEdge(3, 4, 70);
   
-  // Print the adjacency list representation of the graph with weights
-  g.printGraph();
+  cout << "DFS traversal starting from vertex 0: ";
+  g.DFS(0);
+  cout << endl;
+  
+  cout << "BFS traversal starting from vertex 0: ";
+  g.BFS(0);
+  cout << endl;
   
   return 0;
 }
